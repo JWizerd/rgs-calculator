20468880255957761c6aa3285a5a267b01db26e8
Jeremiah Wodke <jeremiah@Jeremiahs-iMac.local>
2018-02-08 10:07:09 -0700

    calculations annual monthly

--- js/systemEstimate.js
+++ js/systemEstimate.js
@@ -4,8 +4,10 @@ var system = {
   systemSize: 0,
   cost: 0,
   solarCostPerWatt: 3.18,
+  monthlySavings: 0,
   annualSavings: 0,
-  systemYearPayOff: 0
+  systemYearPayOff: 0,
+  payoffPeriod: 0,
 
   kwhPerMonth: function() {
 
@@ -21,7 +23,7 @@ var system = {
 
     this.kwhPerYear = Math.floor(this.kwhPerMonth() * 12)
 
-    Cookies.set('annualKwh', this.kwhPerYear.toLocaleString())
+    Cookies.set('annualKwh', this.kwhPerYear.toLocaleString(), { expires: view.expiration })
 
   },
 
@@ -29,7 +31,7 @@ var system = {
 
     this.systemSize = (this.kwhPerMonth / 100).toFixed(1)
 
-    Cookies.set('dcSystemSize', this.systemSize)
+    Cookies.set('dcSystemSize', this.systemSize, { expires: view.expiration })
 
   },
 
@@ -40,7 +42,17 @@ var system = {
     // since the standard federal tax credit each for solar is 30% off the total cost of the system we can add this to total?
     var federalTaxCredit = this.cost * .3
 
-    Cookies.set('cost', Math.floor(this.cost - federalTaxCredit).toLocaleString())
+    Cookies.set('cost', Math.floor(this.cost - federalTaxCredit).toLocaleString(), { expires: view.expiration })
+
+  },
+
+  setMonthlySavings: function () {
+
+    // for now monthly savings is set to what a users monthly utility bill is
+
+    this.monthlySavings = parseInt(Cookies.get('monthlyUtilityRate'))
+
+    Cookies.set('monthlySavings', this.monthlySavings, { expires: view.expiration })
 
   },
 
@@ -48,35 +60,46 @@ var system = {
 
     // Utility Bill * 12 = Savings Year 1
     
-    this.annualSavings = parseFloat(Cookies.get('monthlyUtilityRate')) * 12
+    this.annualSavings = this.monthlySavings * 12
 
-    Cookies.set('annualSavings', Math.floor(this.cost - federalTaxCredit).toLocaleString())
+    Cookies.set('annualSavings', this.annualSavings, { expires: view.expiration })
 
   },
 
   systemYearPayOffCalc: function() {
 
     // (Utility Bill * 12) * Utility Escalator(x) = Savings Year x 
+    this.payoffPeriod = Math.floor(this.cost / this.annualSavings)
     
-    this.systemYearPayOff = this.cost / this.annualSavings
+    this.systemYearPayOff = new Date().getFullYear() + Math.floor(this.cost / this.annualSavings) 
     
-    Cookies.set('systemYearPayOff', this.cost / this
+    Cookies.set('systemYearPayOff', this.systemYearPayOff, { expires: view.expiration })
+    Cookies.set('payoffPeriod', this.payoffPeriod, { expires: view.expiration })
 
   },
 
   render: function() {
 
-    this.annualKwh();
-    this.dcSystemSize();
-    this.cost();
-
-    console.log(this.kwhPerMonth)
+    this.annualKwh()
+    this.dcSystemSize()
+    this.cost()
+    this.setMonthlySavings()
+    this.annualSavingsCalc()
+    this.systemYearPayOffCalc()
 
     setTimeout(function(){
+
+      $('#roof-image').css({
+        'background': 'url(' + Cookies.get('roofImage') +  ') top center / cover no-repeat'
+      })
 
       $('#annual-usage').text(Cookies.get('annualKwh') + 'KWh')
       $('#dc-system-size').text(Cookies.get('dcSystemSize') + 'KWh')
       $('#cost').text('$' + Cookies.get('cost'))
+      $('#monthly-savings').text('$' + Cookies.get('monthlySavings'))
+      $('#annual-savings').text('$' + Cookies.get('annualSavings'))
+      $('#payback-period').text(Cookies.get('payoffPeriod') + ' Years')
+      $('#payback-year').text(Cookies.get('systemYearPayOff'))
 
     }, 1000)
 
